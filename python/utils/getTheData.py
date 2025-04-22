3f140e05e59d8b3fb123a036bb5c7686a3a1c9bd
Ahmed-Mahmoud-M <157695160+Ahmed-Mahmoud-M@users.noreply.github.com>
2025-04-22 11:48:38 +0200

    Update getTheData.py
    to get the data from kaggle

--- python/utils/getTheData.py
+++ python/utils/getTheData.py
@@ -1,41 +1,41 @@
-import kagglehub
-import shutil
-import os
+# import kagglehub
+# import shutil
+# import os
 
 
 
 
 
 
-source_dir = kagglehub.dataset_download(
-        "chethuhn/network-intrusion-dataset",
+# source_dir = kagglehub.dataset_download(
+#         "chethuhn/network-intrusion-dataset",
         
-    )
+#     )
 
 
 
 
-dest_dir = "/home/ahmed/deep-anomaly-ips/python/data/raw"
+# dest_dir = "/home/ahmed/deep-anomaly-ips/python/data/raw"
 
 
-os.makedirs(dest_dir, exist_ok=True)
+# os.makedirs(dest_dir, exist_ok=True)
 
 
-try:
-    for item in os.listdir(source_dir):
-        src_path = os.path.join(source_dir, item)
-        dst_path = os.path.join(dest_dir, item)
+# try:
+#     for item in os.listdir(source_dir):
+#         src_path = os.path.join(source_dir, item)
+#         dst_path = os.path.join(dest_dir, item)
         
-        if os.path.exists(dst_path):
-            print(f"⚠️ Skipping '{item}' (already exists in destination)")
-            continue
+#         if os.path.exists(dst_path):
+#             print(f"⚠️ Skipping '{item}' (already exists in destination)")
+#             continue
             
-        shutil.move(src_path, dst_path)
-        print(f"✅ Moved: {item}")
+#         shutil.move(src_path, dst_path)
+#         print(f"✅ Moved: {item}")
 
-    print("\n🎉 Dataset moved successfully!")
-except Exception as e:
-    print(f"❌ Error: {e}")
+#     print("\n Dataset moved successfully!")
+# except Exception as e:
+#     print(f"Error: {e}")
 
 
 
